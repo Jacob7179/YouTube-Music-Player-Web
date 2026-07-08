2453b407400d1fbfafc6f94a0bb6bf70ab8c1041
Jacob <70430960+Jacob7179@users.noreply.github.com>
2026-07-08 17:07:09 +0800

    Refactor code structure for improved readability and maintainability

--- android_build/README-APK.md
+++ android_build/README-APK.md
@@ -4,15 +4,20 @@ This folder wraps the web app with Capacitor so it can be built as an Android AP
 
 ## Auto Build
 ```powershell
-./run.bat
+./build.bat
 ```
 
 ## Manual Build
 
 ```powershell
+cd android_build
 npm install
 npx cap add android
 npx cap sync android
+
+$sdk = (Get-Location).Path.Replace('\', '\\') + '\\android-sdk'
+"sdk.dir=$sdk" | Set-Content -Encoding ASCII .\android\local.properties
+
 cd android
 .\gradlew.bat assembleDebug
 ```
@@ -21,6 +26,11 @@ The debug APK will be created at:
 
 ```text
 android/app/build/outputs/apk/debug/app-debug.apk
+```
+
+## Clean Folder
+```powershell
+./clean.bat
 ```
 
 The app still needs internet access for YouTube playback, YouTube search, lyrics, translation, and CDN-hosted styles/scripts.
--- android_build/build.bat
+++ android_build/build.bat
@@ -88,57 +88,6 @@ echo Created:
 type "android\local.properties"
 
 REM ------------------------------------------------------------
-REM Prepare Android launcher icon and splash screen files.
-REM ------------------------------------------------------------
-echo.
-echo ==========================================
-echo Preparing app icon and splash screen...
-echo ==========================================
-
-if not exist "assets" (
-    mkdir assets
-)
-
-if not exist "www\resource\icon\app-icon-512.png" (
-    echo.
-    echo ERROR: Icon image not found:
-    echo www\resource\icon\app-icon-512.png
-    goto :error
-)
-
-REM Normal launcher icon
-copy /Y "www\resource\icon\app-icon-512.png" "assets\icon-only.png"
-if errorlevel 1 goto :error
-
-REM Adaptive icon foreground
-copy /Y "www\resource\icon\app-icon-512.png" "assets\icon-foreground.png"
-if errorlevel 1 goto :error
-
-REM Adaptive icon background - required by Android
-copy /Y "www\resource\icon\app-icon-512.png" "assets\icon-background.png"
-if errorlevel 1 goto :error
-
-REM Splash screen
-copy /Y "www\resource\icon\app-icon-512.png" "assets\splash.png"
-if errorlevel 1 goto :error
-
-echo Icon and splash files prepared.
-
-REM ------------------------------------------------------------
-REM Install Capacitor asset generator and create Android assets.
-REM ------------------------------------------------------------
-echo.
-echo ==========================================
-echo Generating Android app icons and splash...
-echo ==========================================
-
-call npm install -D @capacitor/assets
-if errorlevel 1 goto :error
-
-call npx capacitor-assets generate --android
-if errorlevel 1 goto :error
-
-REM ------------------------------------------------------------
 REM Sync latest web files into Android project.
 REM ------------------------------------------------------------
 echo.
@@ -148,6 +97,86 @@ echo ==========================================
 
 call npx cap sync android
 if errorlevel 1 goto :error
+
+REM ------------------------------------------------------------
+REM Replace Android icon and splash resources from image_source\res.
+REM Source path:
+REM android_build\image_source\res
+REM ------------------------------------------------------------
+echo.
+echo ==========================================
+echo Replacing Android app icon and splash resources...
+echo ==========================================
+
+set "IMAGE_RES_DIR=%~dp0image_source\res"
+set "ANDROID_RES_DIR=%~dp0android\app\src\main\res"
+
+if not exist "%IMAGE_RES_DIR%" (
+    echo.
+    echo ERROR: Image resource folder not found:
+    echo %IMAGE_RES_DIR%
+    echo.
+    echo Please place your converted res folder here:
+    echo image_source\res
+    goto :error
+)
+
+if not exist "%ANDROID_RES_DIR%" (
+    echo.
+    echo ERROR: Android res folder not found:
+    echo %ANDROID_RES_DIR%
+    goto :error
+)
+
+REM Copy drawable folders for splash images/backgrounds.
+for %%D in (
+    drawable
+    drawable-v24
+    drawable-land-hdpi
+    drawable-land-mdpi
+    drawable-land-xhdpi
+    drawable-land-xxhdpi
+    drawable-land-xxxhdpi
+    drawable-port-hdpi
+    drawable-port-mdpi
+    drawable-port-xhdpi
+    drawable-port-xxhdpi
+    drawable-port-xxxhdpi
+) do (
+    if exist "%IMAGE_RES_DIR%\%%D" (
+        echo Copying %%D ...
+        if not exist "%ANDROID_RES_DIR%\%%D" mkdir "%ANDROID_RES_DIR%\%%D"
+        xcopy /E /I /Y "%IMAGE_RES_DIR%\%%D\*" "%ANDROID_RES_DIR%\%%D\" >nul
+        if errorlevel 1 goto :error
+    )
+)
+
+REM Copy launcher icon mipmap folders.
+for %%D in (
+    mipmap-anydpi-v26
+    mipmap-hdpi
+    mipmap-mdpi
+    mipmap-xhdpi
+    mipmap-xxhdpi
+    mipmap-xxxhdpi
+) do (
+    if exist "%IMAGE_RES_DIR%\%%D" (
+        echo Copying %%D ...
+        if not exist "%ANDROID_RES_DIR%\%%D" mkdir "%ANDROID_RES_DIR%\%%D"
+        xcopy /E /I /Y "%IMAGE_RES_DIR%\%%D\*" "%ANDROID_RES_DIR%\%%D\" >nul
+        if errorlevel 1 goto :error
+    )
+)
+
+REM Copy icon background color only, without replacing app strings/styles.
+if exist "%IMAGE_RES_DIR%\values\ic_launcher_background.xml" (
+    echo Copying values\ic_launcher_background.xml ...
+    if not exist "%ANDROID_RES_DIR%\values" mkdir "%ANDROID_RES_DIR%\values"
+    copy /Y "%IMAGE_RES_DIR%\values\ic_launcher_background.xml" "%ANDROID_RES_DIR%\values\ic_launcher_background.xml" >nul
+    if errorlevel 1 goto :error
+)
+
+echo Android icon and splash resources replaced.
 
 REM ------------------------------------------------------------
 REM Build debug APK.
